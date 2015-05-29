132e04dc34d1841d2cedc61a23faf05f9ba7064d
ulnda <denis.nagibin@gmail.com>
2015-05-29 11:25:19 +0300

    posts service

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', ['$scope', function($scope){
-	$scope.posts = [
-	  {title: 'post 1', upvotes: 5},
-	  {title: 'post 2', upvotes: 2},
-	  {title: 'post 3', upvotes: 15},
-	  {title: 'post 4', upvotes: 9},
-	  {title: 'post 5', upvotes: 4}
-	];
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
+
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
+	$scope.posts = posts.posts
 
 	$scope.addPost = function(){
 	  if(!$scope.title || $scope.title === '') { return; }
