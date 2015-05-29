a8ebe1b2b69513ab2802a4d13524fe42e439f260
ulnda <denis.nagibin@gmail.com>
2015-05-29 21:08:10 +0300

    Angular Auth methods and Navbar

--- public/javascripts/appAngular.js
+++ public/javascripts/appAngular.js
@@ -21,6 +21,24 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 		      return posts.get($stateParams.id);
 		    }]
 		  }
+		}).state('login', {
+		  url: '/login',
+		  templateUrl: '/login.html',
+		  controller: 'AuthCtrl',
+		  onEnter: ['$state', 'auth', function($state, auth){
+		    if(auth.isLoggedIn()){
+		      $state.go('home');
+		    }
+		  }]
+		}).state('register', {
+			url: '/register',
+			templateUrl: '/register.html',
+			controller: 'AuthCtrl',
+			onEnter: ['$state', 'auth', function($state, auth){
+			  if(auth.isLoggedIn()){
+			    $state.go('home');
+			  }
+			}]
 		});
 
   $urlRouterProvider.otherwise('home');
@@ -61,6 +79,56 @@ app.factory('posts', ['$http', function($http){
 		}
 	}
   return o;
+}]).factory('auth', ['$http', '$window', function($http, $window){
+	var auth = {};
+	
+	auth.saveToken = function (token){
+	  $window.localStorage['flapper-news-token'] = token;
+	};
+
+	auth.getToken = function (){
+	  return $window.localStorage['flapper-news-token'];
+	}
+
+	auth.isLoggedIn = function(){
+	  var token = auth.getToken();
+
+	  if(token){
+	    var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+	    return payload.exp > Date.now() / 1000;
+	  } 
+	  else {
+	    return false;
+	  }
+	};
+
+	auth.currentUser = function(){
+	  if(auth.isLoggedIn()){
+	    var token = auth.getToken();
+	    var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+	    return payload.username;
+	  }
+	};
+
+	auth.register = function(user){
+	  return $http.post('/register', user).success(function(data){
+	    auth.saveToken(data.token);
+	  });
+	};
+
+	auth.logIn = function(user){
+	  return $http.post('/login', user).success(function(data){
+	    auth.saveToken(data.token);
+	  });
+	};
+
+	auth.logOut = function(){
+  	$window.localStorage.removeItem('flapper-news-token');
+	};
+
+  return auth;
 }])
 
 app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
@@ -96,3 +164,29 @@ app.controller('PostsCtrl', ['$scope', 'post', 'posts', function($scope, post, p
 	  posts.upvoteComment(post, comment);
 	};
 }]);
+
+app.controller('NavCtrl', ['$scope', 'auth', function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
+}]);
+
+app.controller('AuthCtrl', ['$scope', '$state', 'auth', function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}])
