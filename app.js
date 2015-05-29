d7f135446e4830a513259a65caf8b68b2e14a9bf
ulnda <denis.nagibin@gmail.com>
2015-05-29 11:15:24 +0300

    User Input

--- app.js
+++ app.js
@@ -8,4 +8,19 @@ app.controller('MainCtrl', ['$scope', function($scope){
 	  {title: 'post 4', upvotes: 9},
 	  {title: 'post 5', upvotes: 4}
 	];
+
+	$scope.addPost = function(){
+	  if(!$scope.title || $scope.title === '') { return; }
+	  $scope.posts.push({
+	    title: $scope.title,
+	    link: $scope.link,
+	    upvotes: 0
+	  });
+	  $scope.title = '';
+	  $scope.link = '';
+	};
+
+	$scope.incrementUpvotes = function(post) {
+	  post.upvotes += 1;
+	};
 }]);
