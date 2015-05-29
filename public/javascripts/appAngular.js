5cbf2ffeb9f83cf055ba9c33db9f6b1e42bca1cd
ulnda <denis.nagibin@gmail.com>
2015-05-29 19:14:25 +0300

    Get post / Add comment / Upvote comment

--- public/javascripts/appAngular.js
+++ public/javascripts/appAngular.js
@@ -15,7 +15,12 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
     .state('posts', {
   		url: '/posts/{id}',
   		templateUrl: '/posts.html',
-  		controller: 'PostsCtrl'
+  		controller: 'PostsCtrl',
+		  resolve: {
+		    post: ['$stateParams', 'posts', function($stateParams, posts) {
+		      return posts.get($stateParams.id);
+		    }]
+		  }
 		});
 
   $urlRouterProvider.otherwise('home');
@@ -38,7 +43,21 @@ app.factory('posts', ['$http', function($http){
 		  return $http.put('/posts/' + post._id + '/upvote')
 		    .success(function(data){
 		      post.upvotes += 1;
-		  });
+		  	});
+		},
+		get: function(id) {
+  		return $http.get('/posts/' + id).then(function(res){
+    		return res.data;
+  		});
+		},
+		addComment: function(id, comment) {
+		  return $http.post('/posts/' + id + '/comments', comment);
+		},
+		upvoteComment: function(post, comment) {
+		  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+		  	.success(function(data){
+					comment.upvotes += 1;
+				});
 		}
 	}
   return o;
@@ -62,16 +81,18 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
 	};
 }]);
 
-app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts){
-	$scope.post = posts.posts[$stateParams.id];
+app.controller('PostsCtrl', ['$scope', 'post', 'posts', function($scope, post, posts){
+	$scope.post = post;
 
 	$scope.addComment = function(){
 	  if($scope.body === '') { return; }
-	  $scope.post.comments.push({
-	    body: $scope.body,
-	    author: 'user',
-	    upvotes: 0
+	  posts.addComment(post._id, { body: $scope.body, author: 'user' }).success(function(comment) {
+	    $scope.post.comments.push(comment);
 	  });
 	  $scope.body = '';
+	};
+
+	$scope.incrementUpvotes = function(comment){
+	  posts.upvoteComment(post, comment);
 	};
 }]);
