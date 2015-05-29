297041db85319123d23cf5104f1efd32063ab17c
ulnda <denis.nagibin@gmail.com>
2015-05-29 16:34:39 +0300

    Angular: creating a new post/getting posts list

--- public/javascripts/appAngular.js
+++ public/javascripts/appAngular.js
@@ -5,7 +5,12 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
-      controller: 'MainCtrl'
+      controller: 'MainCtrl',
+		  resolve: {
+		    postPromise: ['posts', function(posts){
+		      return posts.getAll();
+		    }]
+		  }
     })
     .state('posts', {
   		url: '/posts/{id}',
@@ -16,10 +21,26 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
   $urlRouterProvider.otherwise('home');
 }]);
 
-app.factory('posts', [function(){
+app.factory('posts', ['$http', function($http){
   var o = {
-    posts: []
-  };
+    posts: [],
+		getAll: function() {
+			return $http.get('/posts').success(function(data){
+				angular.copy(data, o.posts);
+			});
+		},
+		create: function(post) {
+		  return $http.post('/posts', post).success(function(data){
+		    o.posts.push(data);
+		  });
+		},
+		upvote: function(post) {
+		  return $http.put('/posts/' + post._id + '/upvote')
+		    .success(function(data){
+		      post.upvotes += 1;
+		  });
+		}
+	}
   return o;
 }])
 
@@ -27,22 +48,17 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
 	$scope.posts = posts.posts
 
 	$scope.addPost = function(){
-	  if(!$scope.title || $scope.title === '') { return; }
-	  $scope.posts.push({
-	    title: $scope.title,
-	    link: $scope.link,
-	    upvotes: 0,
-		  comments: [
-		    {author: 'Joe', body: 'Cool post!', upvotes: 0},
-		    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-		  ]
-	  });
-	  $scope.title = '';
-	  $scope.link = '';
+		if(!$scope.title || $scope.title === '') { return; }
+  	posts.create({
+    	title: $scope.title,
+    	link:  $scope.link,
+  	});
+  	$scope.title = '';
+  	$scope.link = '';
 	};
 
 	$scope.incrementUpvotes = function(post) {
-	  post.upvotes += 1;
+	  posts.upvote(post);
 	};
 }]);
 
